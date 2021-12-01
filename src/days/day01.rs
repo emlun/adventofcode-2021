be640cfc0283cebfb500f8cc2e860f7bef3f9cc4
Emil Lundberg <emil@emlun.se>
2021-12-01 09:15:59 +0100

    Clean up day 1

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -1,32 +1,19 @@
 use crate::common::Solution;
 
 fn solve_a(numbers: &[i32]) -> usize {
-    let mut c = 0;
-    for i in 1..numbers.len() {
-        if numbers[i - 1] < numbers[i] {
-            c += 1;
-        }
-    }
-    c
+    (1..numbers.len())
+        .filter(|i| numbers[i - 1] < numbers[*i])
+        .count()
 }
 
 fn solve_b(numbers: &[i32]) -> usize {
-    let mut c = 0;
-    for i in 3..numbers.len() {
-        if numbers[i - 3] + numbers[i - 2] + numbers[i - 1]
-            < numbers[i - 2] + numbers[i - 1] + numbers[i]
-        {
-            c += 1;
-        }
-    }
-    c
+    (3..numbers.len())
+        .filter(|i| numbers[i - 3] < numbers[*i])
+        .count()
 }
 
 pub fn solve(lines: &[String]) -> Solution {
-    let mut numbers: Vec<i32> = lines
-        .iter()
-        .map(|line| line.parse::<i32>().unwrap())
-        .collect();
+    let numbers: Vec<i32> = lines.iter().map(|line| line.parse().unwrap()).collect();
 
     (solve_a(&numbers).to_string(), solve_b(&numbers).to_string())
 }
