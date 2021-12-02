60a014a17c96ac35fa41dcd7ff7052610ec07057
Emil Lundberg <emil@emlun.se>
2021-12-02 06:12:21 +0100

    Solve day 2b

--- src/days/day02.rs
+++ src/days/day02.rs
@@ -1,24 +1,40 @@
 use crate::common::Solution;
 
-fn solve_a(instructions: &[String]) -> i32 {
-    let mut x: i32 = 0;
+fn solve_a(instructions: &[String]) -> i64 {
+    let mut x: i64 = 0;
     let mut y = 0;
     for ins in instructions {
         let mut splits = ins.split_whitespace();
         match (splits.next(), splits.next()) {
-            (Some("forward"), Some(dx)) => x += dx.parse::<i32>().unwrap(),
-            (Some("down"), Some(dy)) => y += dy.parse::<i32>().unwrap(),
-            (Some("up"), Some(dy)) => y -= dy.parse::<i32>().unwrap(),
+            (Some("forward"), Some(dx)) => x += dx.parse::<i64>().unwrap(),
+            (Some("down"), Some(dy)) => y += dy.parse::<i64>().unwrap(),
+            (Some("up"), Some(dy)) => y -= dy.parse::<i64>().unwrap(),
             _ => unreachable!(),
         }
     }
     x * y
 }
 
-fn solve_b() -> usize {
-    0
+fn solve_b(instructions: &[String]) -> i64 {
+    let mut x: i64 = 0;
+    let mut y = 0;
+    let mut a: i64 = 0;
+    for ins in instructions {
+        let mut splits = ins.split_whitespace();
+        match (splits.next(), splits.next()) {
+            (Some("forward"), Some(dx)) => {
+                let dx = dx.parse::<i64>().unwrap();
+                x += dx;
+                y += a * dx;
+            }
+            (Some("down"), Some(dy)) => a += dy.parse::<i64>().unwrap(),
+            (Some("up"), Some(dy)) => a -= dy.parse::<i64>().unwrap(),
+            _ => unreachable!(),
+        }
+    }
+    x * y
 }
 
 pub fn solve(lines: &[String]) -> Solution {
-    (solve_a(lines).to_string(), solve_b().to_string())
+    (solve_a(lines).to_string(), solve_b(lines).to_string())
 }
