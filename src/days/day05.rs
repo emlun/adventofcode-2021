2098a665fd4873122094ed2faa33a408c3105ada
Emil Lundberg <emil@emlun.se>
2021-12-05 06:47:11 +0100

    Solve day 5b

--- src/days/day05.rs
+++ src/days/day05.rs
@@ -2,19 +2,40 @@ use crate::common::Solution;
 use std::collections::HashMap;
 use std::collections::HashSet;
 
-fn solve_a(segments: Vec<((i64, i64), (i64, i64))>) -> usize {
+fn solve_sub<'a, I: Iterator<Item = &'a ((i64, i64), (i64, i64))>>(segments: I) -> usize {
     let mut point_counts: HashMap<(i64, i64), usize> = HashMap::new();
-    for ((x1, y1), (x2, y2)) in segments
-        .into_iter()
-        .filter(|((x1, y1), (x2, y2))| x1 == x2 || y1 == y2)
-    {
+    for ((x1, y1), (x2, y2)) in segments {
         if x1 == x2 {
-            for pos in (std::cmp::min(y1, y2)..=std::cmp::max(y1, y2)).map(|y| (x1, y)) {
-                *point_counts.entry(pos).or_insert(0) += 1;
+            for (x, y) in (std::cmp::min(*y1, *y2)..=std::cmp::max(*y1, *y2)).map(|y| (x1, y)) {
+                *point_counts.entry((*x, y)).or_insert(0) += 1;
+            }
+        } else if y1 == y2 {
+            for (x, y) in (std::cmp::min(*x1, *x2)..=std::cmp::max(*x1, *x2)).map(|x| (x, y1)) {
+                *point_counts.entry((x, *y)).or_insert(0) += 1;
             }
         } else {
-            for pos in (std::cmp::min(x1, x2)..=std::cmp::max(x1, x2)).map(|x| (x, y1)) {
-                *point_counts.entry(pos).or_insert(0) += 1;
+            let xs = std::cmp::min(*x1, *x2)..=std::cmp::max(*x1, *x2);
+            let ys = std::cmp::min(*y1, *y2)..=std::cmp::max(*y1, *y2);
+            if x1 <= x2 {
+                if y1 <= y2 {
+                    for (x, y) in xs.zip(ys) {
+                        *point_counts.entry((x, y)).or_insert(0) += 1;
+                    }
+                } else {
+                    for (x, y) in xs.zip(ys.rev()) {
+                        *point_counts.entry((x, y)).or_insert(0) += 1;
+                    }
+                }
+            } else {
+                if y1 <= y2 {
+                    for (x, y) in xs.rev().zip(ys) {
+                        *point_counts.entry((x, y)).or_insert(0) += 1;
+                    }
+                } else {
+                    for (x, y) in xs.rev().zip(ys.rev()) {
+                        *point_counts.entry((x, y)).or_insert(0) += 1;
+                    }
+                }
             }
         }
     }
@@ -37,5 +58,13 @@ pub fn solve(lines: &[String]) -> Solution {
         })
         .collect();
 
-    (solve_a(segments).to_string(), 0.to_string())
+    (
+        solve_sub(
+            segments
+                .iter()
+                .filter(|((x1, y1), (x2, y2))| x1 == x2 || y1 == y2),
+        )
+        .to_string(),
+        solve_sub(segments.iter()).to_string(),
+    )
 }
