55aedd3ffe99293bfcdc39fbde7949a03dd90762
Emil Lundberg <emil@emlun.se>
2021-12-07 06:22:30 +0100

    Solve day 7b

--- src/days/day07.rs
+++ src/days/day07.rs
@@ -10,10 +10,22 @@ pub fn solve(lines: &[String]) -> Solution {
 
     let minx: i64 = *crabs.iter().min().unwrap();
     let maxx: i64 = *crabs.iter().max().unwrap();
-    let sol = (minx..=maxx)
+    let sol_a = (minx..=maxx)
         .map(|x0| crabs.iter().map(|x| (*x - x0).abs()).sum::<i64>())
         .min()
         .unwrap();
+    let sol_b = (minx..=maxx)
+        .map(|x0| {
+            crabs
+                .iter()
+                .map(|x| {
+                    let n = (*x - x0).abs();
+                    n * (n + 1) / 2
+                })
+                .sum::<i64>()
+        })
+        .min()
+        .unwrap();
 
-    (sol.to_string(), 0.to_string())
+    (sol_a.to_string(), sol_b.to_string())
 }
