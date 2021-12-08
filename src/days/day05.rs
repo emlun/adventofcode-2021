638e5991fe9e8a16a95a8b88247f4e18efea280a
Emil Lundberg <emil@emlun.se>
2021-12-08 22:10:43 +0100

    Compute day 5a and 5b in parallel

--- src/days/day05.rs
+++ src/days/day05.rs
@@ -1,16 +1,17 @@
 use crate::common::Solution;
 use std::collections::HashMap;
 
-fn solve_sub<'a, I: Iterator<Item = &'a ((i64, i64), (i64, i64))>>(segments: I) -> usize {
-    let mut point_counts: HashMap<(i64, i64), usize> = HashMap::new();
+fn solve_sub<'a, I: Iterator<Item = &'a ((i64, i64), (i64, i64))>>(segments: I) -> (usize, usize) {
+    let mut straight_point_counts: HashMap<(i64, i64), usize> = HashMap::new();
+    let mut diag_point_counts: HashMap<(i64, i64), usize> = HashMap::new();
     for ((x1, y1), (x2, y2)) in segments {
         if x1 == x2 {
             for (x, y) in (std::cmp::min(*y1, *y2)..=std::cmp::max(*y1, *y2)).map(|y| (x1, y)) {
-                *point_counts.entry((*x, y)).or_insert(0) += 1;
+                *straight_point_counts.entry((*x, y)).or_insert(0) += 1;
             }
         } else if y1 == y2 {
             for (x, y) in (std::cmp::min(*x1, *x2)..=std::cmp::max(*x1, *x2)).map(|x| (x, y1)) {
-                *point_counts.entry((x, *y)).or_insert(0) += 1;
+                *straight_point_counts.entry((x, *y)).or_insert(0) += 1;
             }
         } else {
             let xs = std::cmp::min(*x1, *x2)..=std::cmp::max(*x1, *x2);
@@ -18,27 +19,38 @@ fn solve_sub<'a, I: Iterator<Item = &'a ((i64, i64), (i64, i64))>>(segments: I) 
             if x1 <= x2 {
                 if y1 <= y2 {
                     for (x, y) in xs.zip(ys) {
-                        *point_counts.entry((x, y)).or_insert(0) += 1;
+                        *diag_point_counts.entry((x, y)).or_insert(0) += 1;
                     }
                 } else {
                     for (x, y) in xs.zip(ys.rev()) {
-                        *point_counts.entry((x, y)).or_insert(0) += 1;
+                        *diag_point_counts.entry((x, y)).or_insert(0) += 1;
                     }
                 }
             } else {
                 if y1 <= y2 {
                     for (x, y) in xs.rev().zip(ys) {
-                        *point_counts.entry((x, y)).or_insert(0) += 1;
+                        *diag_point_counts.entry((x, y)).or_insert(0) += 1;
                     }
                 } else {
                     for (x, y) in xs.rev().zip(ys.rev()) {
-                        *point_counts.entry((x, y)).or_insert(0) += 1;
+                        *diag_point_counts.entry((x, y)).or_insert(0) += 1;
                     }
                 }
             }
         }
     }
-    point_counts.values().filter(|v| **v >= 2).count()
+
+    for ((x, y), c) in straight_point_counts.iter() {
+        if *c >= 2 {
+            *diag_point_counts.entry((*x, *y)).or_insert(0) += c;
+        } else {
+            diag_point_counts.entry((*x, *y)).and_modify(|cc| *cc += 1);
+        }
+    }
+    (
+        straight_point_counts.values().filter(|v| **v >= 2).count(),
+        diag_point_counts.values().filter(|v| **v >= 2).count(),
+    )
 }
 
 pub fn solve(lines: &[String]) -> Solution {
@@ -57,13 +69,6 @@ pub fn solve(lines: &[String]) -> Solution {
         })
         .collect();
 
-    (
-        solve_sub(
-            segments
-                .iter()
-                .filter(|((x1, y1), (x2, y2))| x1 == x2 || y1 == y2),
-        )
-        .to_string(),
-        solve_sub(segments.iter()).to_string(),
-    )
+    let (sol_a, sol_b) = solve_sub(segments.iter());
+    (sol_a.to_string(), sol_b.to_string())
 }
