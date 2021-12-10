f79e2baca8725c8c4965516fa22053e7863674ca
Emil Lundberg <emil@emlun.se>
2021-12-10 15:31:28 +0100

    Solve day 10b

--- src/days/day10.rs
+++ src/days/day10.rs
@@ -3,7 +3,7 @@ use crate::common::Solution;
 #[derive(Debug)]
 enum Chunk {
     Valid(char, Vec<Chunk>),
-    Incomplete,
+    Incomplete(char, Vec<Chunk>),
     Corrupt(char),
 }
 use Chunk::Corrupt;
@@ -11,37 +11,59 @@ use Chunk::Incomplete;
 use Chunk::Valid;
 
 fn parse_chunk<I: Iterator<Item = char>>(input: &mut std::iter::Peekable<I>) -> Chunk {
-    if let Some(open) = input.next() {
-        match open {
-            '(' | '[' | '{' | '<' => {
-                let mut subchunks = Vec::new();
-                loop {
-                    match input.peek() {
-                        Some(')' | ']' | '}' | '>') => {
-                            let close = input.next().unwrap();
-                            match (open, close) {
-                                ('(', ')') | ('[', ']') | ('{', '}') | ('<', '>') => {
-                                    break Valid(open, subchunks)
-                                }
-                                _ => break Corrupt(close),
-                            };
-                        }
-                        Some(_) => match parse_chunk(input) {
-                            subchunk @ Valid(_, _) => {
-                                subchunks.push(subchunk);
+    let open = input.next().unwrap();
+    match open {
+        '(' | '[' | '{' | '<' => {
+            let mut subchunks = Vec::new();
+            loop {
+                match input.peek() {
+                    Some(')' | ']' | '}' | '>') => {
+                        let close = input.next().unwrap();
+                        match (open, close) {
+                            ('(', ')') | ('[', ']') | ('{', '}') | ('<', '>') => {
+                                break Valid(open, subchunks)
                             }
-                            i @ Incomplete => break i,
-                            c @ Corrupt(_) => break c,
-                        },
-                        None => break Incomplete,
+                            _ => break Corrupt(close),
+                        };
                     }
+                    Some(_) => match parse_chunk(input) {
+                        subchunk @ Valid(..) => {
+                            subchunks.push(subchunk);
+                        }
+                        i @ Incomplete(..) => {
+                            subchunks.push(i);
+                        }
+                        c @ Corrupt(..) => break c,
+                    },
+                    None => break Incomplete(open, subchunks),
                 }
             }
-            other => Corrupt(other),
         }
-    } else {
-        Incomplete
+        other => Corrupt(other),
     }
+}
+
+fn complete_chunk(chunk: &Chunk) -> Vec<char> {
+    fn recurse(chunk: &Chunk, output: &mut Vec<char>) {
+        match chunk {
+            Incomplete(open, subchunks) => {
+                for sub in subchunks {
+                    recurse(sub, output);
+                }
+                output.push(match open {
+                    '(' => ')',
+                    '[' => ']',
+                    '{' => '}',
+                    '<' => '>',
+                    _ => unreachable!(),
+                });
+            }
+            _ => {}
+        }
+    }
+    let mut output = Vec::new();
+    recurse(chunk, &mut output);
+    output
 }
 
 pub fn solve(lines: &[String]) -> Solution {
@@ -62,7 +84,24 @@ pub fn solve(lines: &[String]) -> Solution {
         })
         .sum();
 
-    let sol_b = 0;
+    let mut completion_scores: Vec<usize> = chunks
+        .iter()
+        .filter(|c| matches!(c, Incomplete(..)))
+        .map(|c| {
+            complete_chunk(c).into_iter().fold(0, |score, chr| {
+                score * 5
+                    + match chr {
+                        ')' => 1,
+                        ']' => 2,
+                        '}' => 3,
+                        '>' => 4,
+                        _ => unreachable!(),
+                    }
+            })
+        })
+        .collect();
+    completion_scores.sort();
+    let sol_b = completion_scores[completion_scores.len() / 2];
 
     (sol_a.to_string(), sol_b.to_string())
 }
