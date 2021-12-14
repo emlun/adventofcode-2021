9d4a16ff1bc57ac1c2ba5c6d582aeefa4b59db11
Emil Lundberg <emil@emlun.se>
2021-12-14 18:46:07 +0100

    Model day 14 pairs as int instead of &str, and Vec instead of HashMap

--- src/days/day14.rs
+++ src/days/day14.rs
@@ -1,29 +1,32 @@
 use crate::common::Solution;
 use crate::util::iter::Countable;
 use std::collections::HashMap;
+use std::collections::HashSet;
 
-fn grow<'a>(
-    rules: &HashMap<&'a str, (&'a str, &'a str)>,
-    polymer: HashMap<&'a str, usize>,
-) -> HashMap<&'a str, usize> {
+fn grow(rules: &[(usize, usize)], polymer: HashMap<usize, usize>) -> HashMap<usize, usize> {
     polymer.into_iter().fold(
         HashMap::new(),
-        |mut result, (pair, count): (&str, usize)| {
-            if let Some((p1, p2)) = rules.get(pair) {
-                *result.entry(p1).or_insert(0) += count;
-                *result.entry(p2).or_insert(0) += count;
-            }
+        |mut result, (pair, count): (usize, usize)| {
+            let (p1, p2) = rules[pair];
+            *result.entry(p1).or_insert(0) += count;
+            *result.entry(p2).or_insert(0) += count;
             result
         },
     )
 }
 
-fn count_solution(polymer: &HashMap<&str, usize>, template: &str) -> usize {
+fn count_solution(
+    int_to_name: &HashMap<usize, &&str>,
+    polymer: &HashMap<usize, usize>,
+    template: &str,
+) -> usize {
     let mut elem_counts: HashMap<char, usize> =
         polymer
             .iter()
             .fold(HashMap::new(), |mut counts, (pair, count)| {
-                *counts.entry(pair.chars().next().unwrap()).or_insert(0) += count;
+                *counts
+                    .entry(int_to_name[pair].chars().next().unwrap())
+                    .or_insert(0) += count;
                 counts
             });
     *elem_counts
@@ -51,15 +54,32 @@ pub fn solve(lines: &[String]) -> Solution {
         .iter()
         .map(|(k, (v1, v2))| (*k, (v1.as_str(), v2.as_str())))
         .collect();
+    let pairs: HashSet<&&str> = rules
+        .keys()
+        .chain(
+            rules
+                .values()
+                .flat_map(|(a, b)| Some(a).into_iter().chain(Some(b).into_iter())),
+        )
+        .collect();
+    let int_to_name: HashMap<usize, &&str> = pairs.iter().copied().enumerate().collect();
+    let name_to_int: HashMap<&&str, usize> =
+        pairs.into_iter().enumerate().map(|(i, n)| (n, i)).collect();
+    let rules_int: Vec<(usize, usize)> = (0..=*int_to_name.keys().max().unwrap())
+        .map(|i| {
+            let (a, b) = &rules[int_to_name[&i]];
+            (name_to_int[a], name_to_int[b])
+        })
+        .collect();
 
-    let pair_counts = (0..(template.len() - 1))
-        .map(|i| &template[i..=(i + 1)])
+    let pair_counts: HashMap<usize, usize> = (0..(template.len() - 1))
+        .map(|i| name_to_int[&&template[i..=(i + 1)]])
         .counts();
-    let grown = (0..10).fold(pair_counts, |pair_counts, _| grow(&rules, pair_counts));
-    let sol_a = count_solution(&grown, template);
+    let grown = (0..10).fold(pair_counts, |pair_counts, _| grow(&rules_int, pair_counts));
+    let sol_a = count_solution(&int_to_name, &grown, template);
 
-    let grown = (10..40).fold(grown, |pair_counts, _| grow(&rules, pair_counts));
-    let sol_b = count_solution(&grown, template);
+    let grown = (10..40).fold(grown, |pair_counts, _| grow(&rules_int, pair_counts));
+    let sol_b = count_solution(&int_to_name, &grown, template);
 
     (sol_a.to_string(), sol_b.to_string())
 }
